06315302e494023836e126871f2220bd9d67afd3
Wasgo097 <patrykpakula321@gmail.com>
2020-09-26 20:11:43 +0200

    Health and Mana UI

--- Source/RPGGame/RPGGameCharacter.h
+++ Source/RPGGame/RPGGameCharacter.h
@@ -1,11 +1,8 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "RPGGameCharacter.generated.h"
-
 UCLASS(config=Game)
 class ARPGGameCharacter : public ACharacter{
 	GENERATED_BODY()
@@ -15,19 +12,17 @@ class ARPGGameCharacter : public ACharacter{
 	class UCameraComponent* FollowCamera;
 public:
 	ARPGGameCharacter();
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 protected:
 	void MoveForward(float Value);
 	void MoveRight(float Value);
 	void CameraForward(float Value);
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-public:
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+protected:
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+		float Health = 1.0;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		float Mana = 1.0;
 };
 
